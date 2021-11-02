1660907d9e3d4d4516f3ea38a22f87906bc9337f
schrammbox <schrammbox@gmail.com>
2021-11-02 19:38:51 +0100

    New Index.html Changed style.css Changed MainController.java

--- src/main/java/de/thb/webbakilogin/web/MainPageController.java
+++ src/main/java/de/thb/webbakilogin/web/MainPageController.java
@@ -1,17 +1,43 @@
 package de.thb.webbakilogin.web;
 
+import de.thb.webbakilogin.service.UserService;
 import de.thb.webbakilogin.web.dao.UserLoginDao;
 import de.thb.webbakilogin.web.dao.UserRegistrationDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class MainPageController{
 
-    public String index(){
-        return "Spring Boot Example";
+    /*
+    Giving the Login Parameters for the Login Form in index.html
+     */
+
+    @ModelAttribute("user")
+    public UserLoginDao userLoginDto(){
+        return new UserLoginDao();
     }
+
+    /*
+    Giving the Register Parameters for the Register Form in index.html
+     */
+    @ModelAttribute("user")
+    public UserRegistrationDao userRegistrationDao(){
+        return new UserRegistrationDao();
+    }
+
+    @GetMapping
+    public String showIndex(){
+        return "index";
+    }
+
+    /***
+     * TODO
+     * @ return
+     */
+
 }
