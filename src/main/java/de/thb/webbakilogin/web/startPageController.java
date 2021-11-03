a6d832b1420a5e759a3f9817eb84412d2c992b1a
schrammbox <schrammbox@gmail.com>
2021-11-03 17:05:11 +0100

    Navbar removed

---
+++ src/main/java/de/thb/webbakilogin/web/startPageController.java
@@ -0,0 +1,9 @@
+package de.thb.webbakilogin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/start")
+public class startPageController {
+}
