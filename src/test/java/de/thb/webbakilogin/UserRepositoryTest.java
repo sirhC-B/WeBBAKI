c294334fcf0893f5e2131f351b99f1f9247b0e19
sirhC-B <chris_boesener@gmx.net>
2021-11-03 18:04:46 +0100

    add createUser Junittest

--- src/test/java/de/thb/webbakilogin/UserRepositoryTest.java
+++ src/test/java/de/thb/webbakilogin/UserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -32,5 +33,6 @@ public class UserRepositoryTest {
 
         User existUser = entityManager.find(User.class, savedUser.getId());
 
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
     }
 }
