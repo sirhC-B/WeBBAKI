928bd2a62dda0e5b3198f4594b04e890a1236a5a
sirhC-B <chris_boesener@gmx.net>
2021-11-22 14:33:19 +0100

    LOGIN - REGISTRATION MERGE 2.0

--- src/main/java/de/thb/webbakilogin/config/WebSecurityConfig.java
+++ src/main/java/de/thb/webbakilogin/config/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/start")
                 .permitAll()
                 .and()
                 .logout()
--- src/main/java/de/thb/webbakilogin/web/LoginController.java
+++ src/main/java/de/thb/webbakilogin/web/LoginController.java
@@ -1,10 +1,14 @@
 package de.thb.webbakilogin.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
+
+
 
     @GetMapping("/user")
     public String user() {
